0e5a6d5b96e33e3c1aab4bc8f1ff6c7fb0e7d479
maxv3494 <mvera35@uabc.edu.mx>
2020-05-18 14:25:55 -0700

    Aplicación con vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,73 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+
+      <v-btn icon @click="menu=!menu">
+        <v-icon>mdi-view-headline</v-icon>
+      </v-btn>
+      <div class="d-flex align-center">
+      </div>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>Java Script para Clientes y Servidores</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-navigation-drawer app v-model="menu" color="primary">
+      <v-list dense nav>
+
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-school</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>
+              <v-tab to="/estudiantes">
+                Estudiantes
+              </v-tab>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-chair-school</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>
+              <v-tab to="/cursos">
+                Cursos
+              </v-tab>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <router-view>
+      </router-view>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  components: {
+  },
+  data () {
+    return {
+      menu: false
+    }
+  }
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import TablaAlumnos from '../views/estudiantes.vue'
+import TablaCursos from '../views/cursos.vue'
 
 Vue.use(VueRouter)
 
@@ -8,15 +9,17 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    redirect: '/estudiantes'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/estudiantes',
+    component: TablaAlumnos,
+    name: 'estudiantes'
+  },
+  {
+    path: '/cursos',
+    name: 'cursos',
+    component: TablaCursos
   }
 ]
 
